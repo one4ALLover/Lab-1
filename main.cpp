8ef89e49ae9f8ea9a5ef65cb0d93a7c3103767fc
one4ALLover <egorrvanijked@gmail.com>
2024-09-10 12:35:26 +0300

    reworked 5th task

--- main.cpp
+++ main.cpp
@@ -3,18 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <locale.h>
+#include <windows.h>
 #include <limits.h>
 #include <time.h>
 
-#define NAME 15
-#define MAX_S 50
+#define NAME 17
+#define MAX_S 5
+
+//       ---------------5-----------------
 
 struct student {
     char Name[NAME];
     char Name2[NAME];
     int Age, Nomzach;
-}students[MAX_S];
+}
+students[MAX_S] = {
+    {"Иван", "Пичаев", 18, 12345},
+    {"Сергей", "Чупраков", 19, 12346},
+    {"Валерий", "Кочегин", 18, 12347},
+    {"Илья", "Ваняшин", 19, 12348},
+    {"Никита", "Лунтев", 19, 12349}
+};
 
 void searchByName(struct student students[], int c, const char* Name, const char* Name2) {
     int found = 0;
@@ -51,30 +60,33 @@ void searchByNomzach(struct student students[], int c, int Nomzach) {
         }
     }
     if (!found) {
-        printf("Студента с таким возрастом нет :/\n");
+        printf("Студента с таким  нет номером зачетки нет:/\n");
     }
 }
 
 int main() {
 
-    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-    int c = 0;
+    int c = MAX_S;
 
-    printf("Количество студентов: ");
-    scanf("%d", &c);
+    //for (int i = 0; i < c; i++) {
+    //    printf("Имя студента %d: ", i + 1);
+    //    scanf("%s", students[i].Name);
+    //    printf("Фамилия студента %d: ", i + 1);
+    //    scanf("%s", students[i].Name2);
+    //    printf("Возраст студента %d: ", i + 1);
+    //    scanf("%d", &students[i].Age);
+    //    printf("Ночер зачетки студента %d: ", i + 1);
+    //    scanf("%d", &students[i].Nomzach);
+    //}
+
     for (int i = 0; i < c; i++) {
-        printf("Имя студента %d: ", i + 1);
-        scanf("%s", students[i].Name);
-        printf("Фамилия студента %d: ", i + 1);
-        scanf("%s", students[i].Name2);
-        printf("Возраст студента %d: ", i + 1);
-        scanf("%d", &students[i].Age);
-        printf("Ночер зачетки студента %d: ", i + 1);
-        scanf("%d", &students[i].Nomzach);
+        printf("Студент: %s %s. Возраст: %d. Номер зачетки: %d.\n", students[i].Name, students[i].Name2, students[i].Age, students[i].Nomzach);
     }
 
-    int choice;
+    char choice = '0';
     do {
         printf("\nВыберите метод поиска:\n");
         printf("1. Поиск по имени и фамилии\n");
@@ -82,10 +94,12 @@ int main() {
         printf("3. Поиск по номеру зачетки\n");
         printf("4. Выход\n");
         printf("Ваш выбор: ");
-        scanf("%d", &choice);
+        scanf("%s", &choice);
+
 
         switch (choice) {
-        case 1: {
+
+        case '1': {
             char search1[NAME];
             char search2[NAME];
             printf("Введите имя для поиска: ");
@@ -95,26 +109,30 @@ int main() {
             searchByName(students, c, search1, search2);
             break;
         }
-        case 2: {
+        case '2': {
             int searchAge;
             printf("Введите возраст для поиска: ");
             scanf("%d", &searchAge);
             searchByAge(students, c, searchAge);
             break;
         }
-        case 3: {
+        case '3': {
             int searchNomzach;
             printf("Введите номер зачетки для поиска: ");
             scanf("%d", &searchNomzach);
             searchByNomzach(students, c, searchNomzach);
             break;
         }
-        case 4:
+        case '4': {
             printf("Выход из программы.\n");
+            exit(choice);
             break;
-        default:
-            printf("Неверный выбор. Попробуйте снова.\n");
         }
+        default: {
+            printf("Неверный выбор. Попробуйте снова.\n");
+            break; 
+        }
+      }
     } while (choice != 4);
 
     return 0;
