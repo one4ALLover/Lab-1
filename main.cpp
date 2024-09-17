cccc39b32b408e5d593e5872a626c71a0b7120d5
one4ALLover <egorrvanijked@gmail.com>
2024-09-17 11:52:38 +0300

    add report

--- main.cpp
+++ main.cpp
@@ -14,18 +14,18 @@ int main() {
 
     int mi3 = 0, ma3 = 5;
     int sum_str = 0, sum_sto = 0;
-    int n, m, k;
+    int strs, tabs, k;
     printf("Введите размер массива через пробел): ");
-    scanf_s("%d %d", &n, &m);
-    int** h = (int**)malloc(n * sizeof(int*));
+    scanf_s("%d %d", &strs, &tabs);
+    int** h = (int**)malloc(strs * sizeof(int*));//массив массивов с указателями, его создание 
     srand(time(NULL));
 
-    for (int i = 0; i < n; i++) {
-        h[i] = (int*)malloc(m * sizeof(int));
-    }
+    for (int i = 0; i < strs; i++) {
+        h[i] = (int*)malloc(tabs * sizeof(int));
+    } //
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 0; i < strs; i++) {
+        for (int j = 0; j < tabs; j++) {
             h[i][j] = mi3 + rand() % (ma3 - mi3 + 1);
             printf("%d\t", h[i][j]);
         }
@@ -33,23 +33,35 @@ int main() {
     }
 
     printf("\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < strs; i++) {
         sum_str = 0;
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < tabs; j++) {
             sum_str += h[i][j];
         }
         printf("Сумма %d строки: %d\n", i + 1, sum_str);
     }
 
     printf("\n");
-    for (int j = 0; j < m; j++) {
+    for (int j = 0; j < tabs; j++) {
         sum_sto = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < strs; i++) {
             sum_sto += h[i][j];
         }
         printf("Сумма %d столбца: %d\n", j + 1, sum_sto);
     }
 
+    int sum_task = 0;
+    printf("\n");
+    for (int i = 0; i < strs - 1; ++i)
+    {
+        for (int j = i + 1; j < tabs; ++j)
+        {
+            if (h[i][j] % 2==0) {
+                sum_task += *(*(h + i) + j);
+            }
+        }
+    }
+    printf("Сумма элементов выше главной диагонали: %d\n", sum_task);
     return 0;
 }
-//задание: в квад динам массиве посчитать сумму эл-ов кратных двум выше главной диагонали
+//задание: в квадратном динам массиве посчитать сумму эл-ов кратных двум выше главной диагонали
